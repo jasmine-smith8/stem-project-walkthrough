f29b2986fd6f4efc697bab2daf309001bce5e1ab
cerys-pinch <124775970+cerys-pinch@users.noreply.github.com>
2026-02-19 13:30:14 +0000

    Update rest/router.py
    Co-authored-by: nicolemurch <nicole.murch@hpe.com>

--- rest/router.py
+++ rest/router.py
@@ -4,7 +4,7 @@ from flask import Flask
 from .home import home_route
 # TODO: Import the get_route function from the get_fact module
 # TODO: (Task P1.3) Import the create_route function from the create_fact module
-# TODO: Import the vote_fact function from the vote_fact module
+# TODO: (Task P3.3) Import the vote_fact function from the vote_fact module
 
 def create_app():
     app = Flask(__name__,
